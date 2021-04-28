9f98b66227ca6b45a53de1be92e8775005691874
dado93 <davide.marzorati.93@gmail.com>
2021-04-28 15:31:55 +0200

    LIS3DH Register Map and Overrun/New data functions.
    A register map header file for the LIS3DH accelerometer was added.
    In the LIS3DH component, check on overrun and new data functions for
    both axis and ADC were implemented.

--- Sensors.cylib/LIS3DH/API/LIS3DH.c
+++ Sensors.cylib/LIS3DH/API/LIS3DH.c
@@ -1,5 +1,5 @@
 /**
-  *  \file           LIS3DH.c
+  *  \file           `$INSTANCE_NAME`.c
   *  \brief          Source file for LIS3DH interface.
   *
   *  \author         Davide Marzorati
@@ -30,20 +30,32 @@
 *******************************************************************************/
 
 #include "`$INSTANCE_NAME`.h"
+#include "`$INSTANCE_NAME`_RegMap.h"
 #include "`$INSTANCE_NAME`_I2C.h"
 
 /***********************************
 *          API Constants           *
 ************************************/
+
 /**
  *  \brief          WHO AM I register value.
  */
-#define `$INSTANCE_NAME`_WHO_AM_I_VALUE         0x33
+#define `$INSTANCE_NAME`_WHO_AM_I_VALUE             0x33
 
-/**
- *  \brief          WHO AM I register address.
- */
-#define `$INSTANCE_NAME`_WHO_AM_I_REGISTER      0x0F
+#define `$INSTANCE_NAME`_X_AXIS_PARAM               0x01
+#define `$INSTANCE_NAME`_Y_AXIS_PARAM               0x02
+#define `$INSTANCE_NAME`_Z_AXIS_PARAM               0x03
+#define `$INSTANCE_NAME`_ALL_AXIS_PARAM             0x04
+
+#define `$INSTANCE_NAME`_ADC_CH_1_PARAM             0x01
+#define `$INSTANCE_NAME`_ADC_CH_2_PARAM             0x02
+#define `$INSTANCE_NAME`_ADC_CH_3_PARAM             0x03
+#define `$INSTANCE_NAME`_ADC_ALL_CH_PARAM           0x04
+/***********************************
+*            Macros                *
+************************************/
+#define `$INSTANCE_NAME`_ClearBit(value, bit_pos)   value = 
+
 
 /***********************************
 *     Function Decalarations       *
@@ -58,6 +70,14 @@ static uint8_t `$INSTANCE_NAME`_ReadMulti(uint8_t register_address,
 
 static uint8_t `$INSTANCE_NAME`_Write(uint8_t register_address,
                                         uint8_t value);
+
+static uint8_t `$INSTANCE_NAME`_CheckAxisOverrun(uint8_t axis, uint8_t* overrun);
+
+static uint8_t `$INSTANCE_NAME`_CheckAxisNewData(uint8_t axis, uint8_t* new_data);
+
+static uint8_t `$INSTANCE_NAME`_CheckADCOverrun(uint8_t axis, uint8_t* overrun);
+
+static uint8_t `$INSTANCE_NAME`_CheckADCNewData(uint8_t axis, uint8_t* new_data);
 
 /***********************************
 *          Generic Functions       *
@@ -78,7 +98,7 @@ uint8_t `$INSTANCE_NAME`_Start(void)
     `$INSTANCE_NAME`_I2C_Start();
     
     /* Check WHO AM I register value */
-    uint8_t who_am_i = 0;
+    uint8_t who_am_i, error;
     if (`$INSTANCE_NAME`_ReadWhoAmI(&who_am_i) == `$INSTANCE_NAME`_OK)
     {
         if (who_am_i != `$INSTANCE_NAME`_WHO_AM_I_VALUE)
@@ -94,18 +114,20 @@ uint8_t `$INSTANCE_NAME`_Start(void)
     /* Check if we need to disconnect pull-up */
     if (`$I2C_Pullup`)
     {
-        `$INSTANCE_NAME`_DisconnectPullUp();
+        error = `$INSTANCE_NAME`_DisconnectPullUp();
     }
     else
     {
-        `$INSTANCE_NAME`_ConnectPullUp();
+        error =`$INSTANCE_NAME`_ConnectPullUp();
     }
     
+    return error;
 }
 
 uint8_t `$INSTANCE_NAME`_Stop(void)
 {
     `$INSTANCE_NAME`_I2C_Stop();
+    return `$INSTANCE_NAME`_OK;
 }
 
 /***********************************
@@ -120,14 +142,362 @@ uint8_t `$INSTANCE_NAME`_ReadWhoAmI(uint8_t* value)
 /***********************************
 *     Configuration Functions      *
 ************************************/
+/**
+ *  \brief          Disconnect Pull-Up resistor from SDO/SA0 pin.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ *  \retval         #`$INSTANCE_NANE`_I2C_ERR if error occurred during I2C communication.
+ */
 uint8_t `$INSTANCE_NAME`_DisconnectPullUp(void)
 {
-    
+    /* Read register value */
+    uint8_t temp_reg_value, error;
+    error = `$INSTANCE_NAME`_Read(`$INSTANCE_NAME`_CTRL_REG0_REGISTER, &temp_reg_value);
+    if ( error == `$INSTANCE_NAME`_OK)
+    {
+        /* Set bit 7 of the register */
+        temp_reg_value |= 1 << 7;
+        error = `$INSTANCE_NAME`_Write(`$INSTANCE_NAME`_CTRL_REG0_REGISTER, temp_reg_value);
+        
+        /* Return error based on check */
+        if ( error == `$INSTANCE_NAME`_OK)
+        {
+            return error;
+        }
+        else
+        {
+            return `$INSTANCE_NAME`_I2C_ERR;
+        }
+        
+    }
+    return `$INSTANCE_NAME`_DEV_NOT_FOUND;
 }
 
+/**
+ *  \brief          Connect Pull-Up resistor to SDO/SA0 pin.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ *  \retval         #`$INSTANCE_NANE`_I2C_ERR if error occurred during I2C communication.
+ */
 uint8_t `$INSTANCE_NAME`_ConnectPullUp(void)
 {
-    
+    /* Read register value */
+    uint8_t temp_reg_value, error;
+    error = `$INSTANCE_NAME`_Read(`$INSTANCE_NAME`_CTRL_REG0_REGISTER, &temp_reg_value);
+    if ( error == `$INSTANCE_NAME`_OK)
+    {
+        /* Clear bit 7 of the register */
+        temp_reg_value &= ~(1 << 7);
+        error = `$INSTANCE_NAME`_Write(`$INSTANCE_NAME`_CTRL_REG0_REGISTER, temp_reg_value);
+        
+        /* Return error based on check */
+        if ( error == `$INSTANCE_NAME`_OK)
+        {
+            return error;
+        }
+        else
+        {
+            return `$INSTANCE_NAME`_I2C_ERR;
+        }
+        
+    }
+    return `$INSTANCE_NAME`_DEV_NOT_FOUND;
+}
+
+/***********************************
+*      Axis Overrun Functions      *
+************************************/
+
+/**
+ *  \brief          Check if X axis has overrun.
+ *  \param[out]     overrun : 1 if X axis has ovverun, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_XAxisHasOverrun(uint8_t* overrun)
+{
+    return `$INSTANCE_NAME`_CheckAxisOverrun(`$INSTANCE_NAME`_X_AXIS_PARAM, overrun);
+}
+
+/**
+ *  \brief          Check if Y axis has overrun.
+ *  \param[out]     overrun : 1 if Y axis has ovverun, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_YAxisHasOverrun(uint8_t* overrun)
+{
+    return `$INSTANCE_NAME`_CheckAxisOverrun(`$INSTANCE_NAME`_Y_AXIS_PARAM , overrun);
+}
+
+/**
+ *  \brief          Check if Z axis has overrun.
+ *  \param[out]     overrun : 1 if Z axis has ovverun, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_ZAxisHasOverrun(uint8_t* overrun)
+{
+    return `$INSTANCE_NAME`_CheckAxisOverrun(`$INSTANCE_NAME`_Z_AXIS_PARAM , overrun);
+}
+
+/**
+ *  \brief          Check if any axis has overrun.
+ *  \param[out]     overrun : 1 if any axis has ovverun, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_AxisHasOverrun(uint8_t* overrun)
+{
+    return `$INSTANCE_NAME`_CheckAxisOverrun(`$INSTANCE_NAME`_ALL_AXIS_PARAM , overrun);
+}
+
+/**
+ *  \brief          Check for axis overrun.
+ *  \param[in]      axis: the axis to be checked (1 for X axis, 4 for any axis)
+ *  \param[out]     overrun : 1 if any axis has ovverun, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ *  \retval         #`$INSTANCE_NANE`_CONF_ERR if \ref axis is out of range
+ */
+static uint8_t `$INSTANCE_NAME`_CheckAxisOverrun(uint8_t axis, uint8_t* overrun)
+{
+    if ( axis > `$INSTANCE_NAME`_ALL_AXIS_PARAM )
+    {
+        return `$INSTANCE_NAME`_CONF_ERR;
+    }
+    /* Read register value */
+    uint8_t temp_reg_value, error;
+    error = `$INSTANCE_NAME`_Read(`$INSTANCE_NAME`_STATUS_REGISTER, &temp_reg_value);
+    if ( error == `$INSTANCE_NAME`_OK )
+    {
+        /* Check bit based on axis parameter */
+        *overrun = (temp_reg_value & (1<<(3+axis))) > 0 ? 1 : 0;
+        return error;
+    }   
+    return `$INSTANCE_NAME`_DEV_NOT_FOUND;
+}
+
+/***********************************
+*      ADC Overrun Functions       *
+************************************/
+
+/**
+ *  \brief          Check if ADC Channel 1 has overrun.
+ *  \param[out]     overrun : 1 if ADC Channel 1 has ovverun, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_ADC1HasOverrun(uint8_t* overrun)
+{
+    return `$INSTANCE_NAME`_CheckADCOverrun(`$INSTANCE_NAME`_ADC_CH_1_PARAM, overrun);
+}
+
+/**
+ *  \brief          Check if ADC Channel 2 has overrun.
+ *  \param[out]     overrun : 1 if ADC Channel 2 has ovverun, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_ADC2HasOverrun(uint8_t* overrun)
+{
+    return `$INSTANCE_NAME`_CheckADCOverrun(`$INSTANCE_NAME`_ADC_CH_2_PARAM, overrun);
+}
+
+/**
+ *  \brief          Check if ADC Channel 3 has overrun.
+ *  \param[out]     overrun : 1 if ADC Channel 3 has ovverun, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_ADC3HasOverrun(uint8_t* overrun)
+{
+    return `$INSTANCE_NAME`_CheckADCOverrun(`$INSTANCE_NAME`_ADC_CH_3_PARAM, overrun);
+}
+
+/**
+ *  \brief          Check if any ADC Channel has overrun.
+ *  \param[out]     overrun : 1 if any ADC Channel has ovverun, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_ADCHasOverrun(uint8_t* overrun)
+{
+    return `$INSTANCE_NAME`_CheckADCOverrun(`$INSTANCE_NAME`_ADC_ALL_CH_PARAM, overrun);
+}
+
+/**
+ *  \brief          Check for ADC overrun.
+ *  \param[in]      ch: the channel to be checked (1 for ADC Channel 1, 4 for any channel)
+ *  \param[out]     overrun : 1 if any channel has ovverun, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ *  \retval         #`$INSTANCE_NANE`_CONF_ERR if \ref axis is out of range
+ */
+static uint8_t `$INSTANCE_NAME`_CheckADCOverrun(uint8_t ch, uint8_t* overrun)
+{
+    if (ch > `$INSTANCE_NAME`_ADC_ALL_CH_PARAM)
+    {
+        return `$INSTANCE_NAME`_CONF_ERR;
+    }
+    /* Read register value */
+    uint8_t temp_reg_value, error;
+    error = `$INSTANCE_NAME`_Read(`$INSTANCE_NAME`_STATUS_REG_AUX_REGISTER, &temp_reg_value);
+    if ( error == `$INSTANCE_NAME`_OK)
+    {
+        /* Check bit based on axis parameter */
+        *overrun = (temp_reg_value & (1<<(3+ch))) > 0 ? 1 : 0;
+        return error;
+    }   
+    return `$INSTANCE_NAME`_DEV_NOT_FOUND;
+}
+
+/***********************************
+*      Axis New Data Functions     *
+************************************/
+
+/**
+ *  \brief          Check if X axis has new data.
+ *  \param[out]     overrun : 1 if X axis has new data, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_XAxisDHasNewData(uint8_t* new_data)
+{
+    return `$INSTANCE_NAME`_CheckAxisNewData(`$INSTANCE_NAME`_X_AXIS_PARAM, new_data);
+}
+
+/**
+ *  \brief          Check if Y axis has new data.
+ *  \param[out]     new_data : 1 if Y axis has new data, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_YAxisHasNewData(uint8_t* new_data)
+{
+    return `$INSTANCE_NAME`_CheckAxisNewData(`$INSTANCE_NAME`_Y_AXIS_PARAM, new_data);
+}
+
+/**
+ *  \brief          Check if Z axis has new data.
+ *  \param[out]     overrun : 1 if Z axis has new data, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_ZAxisHasNewData(uint8_t* new_data)
+{
+    return `$INSTANCE_NAME`_CheckAxisNewData(`$INSTANCE_NAME`_Z_AXIS_PARAM, new_data);
+}
+
+/**
+ *  \brief          Check if any axis has new data.
+ *  \param[out]     new_data : 1 if any axis has new data, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_AxisHasNewData(uint8_t* new_data)
+{
+    return `$INSTANCE_NAME`_CheckAxisNewData(`$INSTANCE_NAME`_ALL_AXIS_PARAM, new_data);
+}
+
+/**
+ *  \brief          Check for axis new data.
+ *  \param[in]      axis: the axis to be checked (1 for axis 1, 4 for any axis)
+ *  \param[out]     new_data : 1 if any axis has new data, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ *  \retval         #`$INSTANCE_NANE`_CONF_ERR if \ref axis is out of range
+ */
+static uint8_t `$INSTANCE_NAME`_CheckAxisNewData(uint8_t axis, uint8_t* new_data)
+{
+    if ( axis > `$INSTANCE_NAME`_ALL_AXIS_PARAM )
+    {
+        return `$INSTANCE_NAME`_CONF_ERR;
+    }
+    /* Read register value */
+    uint8_t temp_reg_value, error;
+    error = `$INSTANCE_NAME`_Read(`$INSTANCE_NAME`_STATUS_REGISTER, &temp_reg_value);
+    if ( error == `$INSTANCE_NAME`_OK )
+    {
+        /* Check bit based on axis parameter */
+        *new_data = (temp_reg_value & (1 << axis)) > 0 ? 1 : 0;
+        return error;
+    }   
+    return `$INSTANCE_NAME`_DEV_NOT_FOUND;
+}
+
+/***********************************
+*      ADC New Data Functions     *
+************************************/
+
+/**
+ *  \brief          Check if ADC Channel 1 has new data.
+ *  \param[out]     overrun : 1 if ADC Channel 1 has new data, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_ADC1HasNewData(uint8_t* new_data)
+{
+    return `$INSTANCE_NAME`_CheckADCNewData(`$INSTANCE_NAME`_ADC_CH_1_PARAM, new_data);
+}
+
+/**
+ *  \brief          Check if ADC Channel 2 has new data.
+ *  \param[out]     new_data : 1 if ADC Channel 2 has new data, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_ADC2HasNewData(uint8_t* new_data)
+{
+    return `$INSTANCE_NAME`_CheckADCNewData(`$INSTANCE_NAME`_ADC_CH_2_PARAM, new_data);
+}
+
+/**
+ *  \brief          Check if ADC Channel 3 has new data.
+ *  \param[out]     overrun : 1 if ADC Channel 3 has new data, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_ADC3HasNewData(uint8_t* new_data)
+{
+    return `$INSTANCE_NAME`_CheckADCNewData(`$INSTANCE_NAME`_ADC_CH_3_PARAM, new_data);
+}
+
+/**
+ *  \brief          Check if any ADC channel has new data.
+ *  \param[out]     new_data : 1 if any ADC channel has new data, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ */
+uint8_t `$INSTANCE_NAME`_ADCHasNewData(uint8_t* new_data)
+{
+    return `$INSTANCE_NAME`_CheckADCNewData(`$INSTANCE_NAME`_ADC_ALL_CH_PARAM, new_data);
+}
+
+/**
+ *  \brief          Check for ADC new data.
+ *  \param[in]      ch: the ADC channel to be checked (1 for Channel 1 1, 4 for any channel)
+ *  \param[out]     new_data : 1 if any axis has new data, 0 otherwise.
+ *  \retval         #`$INSTANCE_NAME`_OK if no error occurred.
+ *  \retval         #`$INSTANCE_NAME`_DEV_NOT_FOUND if device was not found on the bus.
+ *  \retval         #`$INSTANCE_NANE`_CONF_ERR if \ref axis is out of range
+ */
+static uint8_t `$INSTANCE_NAME`_CheckADCNewData(uint8_t ch, uint8_t* new_data)
+{
+    if (ch > `$INSTANCE_NAME`_ADC_ALL_CH_PARAM)
+    {
+        return `$INSTANCE_NAME`_CONF_ERR;
+    }
+    /* Read register value */
+    uint8_t temp_reg_value, error;
+    error = `$INSTANCE_NAME`_Read(`$INSTANCE_NAME`_STATUS_REG_AUX_REGISTER, &temp_reg_value);
+    if ( error == `$INSTANCE_NAME`_OK)
+    {
+        /* Check bit based on axis parameter */
+        *new_data = (temp_reg_value & (1 << ch)) > 0 ? 1 : 0;
+        return error;
+    }   
+    return `$INSTANCE_NAME`_DEV_NOT_FOUND;
 }
 
 /***********************************
---
+++ Sensors.cylib/LIS3DH/API/LIS3DH_RegMap.h
@@ -0,0 +1,255 @@
+/**
+  *  \file           `$INSTANCE_NAME`_RegMap.h
+  *  \brief          Map of `$INSTANCE_NAME` I2C registers.
+  *
+  *  \author         Davide Marzorati
+  */
+ 
+/*******************************************************************************
+ * Copyright (c) 2021 Marzorati Davide
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
+
+
+#ifndef __`$INSTANCE_NAME`_REGMAP_H__
+    #define __`$INSTANCE_NAME`_REGMAP_H__
+    
+    #ifdef __cpluscplus 
+        extern "C" {
+    #endif
+
+    /**
+     *  \brief          Status register aux address.
+     */
+    #define `$INSTANCE_NAME`_STATUS_REG_AUX_REGISTER    0x07
+
+    /**
+     *  \brief          Out ADC1 low byte register aux address.
+     */
+    #define `$INSTANCE_NAME`_OUT_ADC_1_L_REGISTER       0x08
+
+    /**
+     *  \brief          Out ADC1 high byte register aux address.
+     */
+    #define `$INSTANCE_NAME`_OUT_ADC_1_H_REGISTER       0x09
+
+    /**
+     *  \brief          Out ADC2 low byte register aux address.
+     */
+    #define `$INSTANCE_NAME`_OUT_ADC_2_L_REGISTER       0x0A
+
+    /**
+     *  \brief          Out ADC2 high byte register aux address.
+     */
+    #define `$INSTANCE_NAME`_OUT_ADC_2_H_REGISTER       0x0B
+
+    /**
+     *  \brief          Out ADC3 low byte register aux address.
+     */
+    #define `$INSTANCE_NAME`_OUT_ADC_3_L_REGISTER       0x0C
+
+    /**
+     *  \brief          Out ADC3 high byte register aux address.
+     */
+    #define `$INSTANCE_NAME`_OUT_ADC_3_H_REGISTER       0x0D
+
+    /**
+     *  \brief          WHO AM I register address.
+     */
+    #define `$INSTANCE_NAME`_WHO_AM_I_REGISTER          0x0F
+
+    /**
+     *  \brief          Control register 0 register address.
+     */
+    #define `$INSTANCE_NAME`_CTRL_REG0_REGISTER         0x1E
+
+    /**
+     *  \brief          Temperature configuration register address.
+     */
+    #define `$INSTANCE_NAME`_TEMP_CFG_REG_REGISTER      0x1F
+
+    /**
+     *  \brief          Control register 1 register address.
+     */
+    #define `$INSTANCE_NAME`_CTRL_REG1_REGISTER         0x20
+
+    /**
+     *  \brief          Control register 2 register address.
+     */
+    #define `$INSTANCE_NAME`_CTRL_REG2_REGISTER         0x21
+
+    /**
+     *  \brief          Control register 3 register address.
+     */
+    #define `$INSTANCE_NAME`_CTRL_REG3_REGISTER         0x22
+
+    /**
+     *  \brief          Control register 4 register address.
+     */
+    #define `$INSTANCE_NAME`_CTRL_REG4_REGISTER         0x23
+    
+    /**
+     *  \brief          Control register 5 register address.
+     */
+    #define `$INSTANCE_NAME`_CTRL_REG5_REGISTER         0x24
+    
+    /**
+     *  \brief          Control register 6 register address.
+     */
+    #define `$INSTANCE_NAME`_CTRL_REG6_REGISTER         0x25
+    
+    /**
+     *  \brief          Control register 6 register address.
+     */
+    #define `$INSTANCE_NAME`_REFERENCE_REGISTER         0x26
+    
+    /**
+     *  \brief          Status register address.
+     */
+    #define `$INSTANCE_NAME`_STATUS_REGISTER            0x27
+    
+    /**
+     *  \brief          Out X low register address.
+     */
+    #define `$INSTANCE_NAME`_OUT_X_L_REGISTER           0x28
+    
+    /**
+     *  \brief          Out X high register address.
+     */
+    #define `$INSTANCE_NAME`_OUT_X_H_REGISTER           0x29
+    
+    /**
+     *  \brief          Out Y low register address.
+     */
+    #define `$INSTANCE_NAME`_OUT_Y_L_REGISTER           0x2A
+    
+    /**
+     *  \brief          Out Y high register address.
+     */
+    #define `$INSTANCE_NAME`_OUT_Y_H_REGISTER           0x2B
+    
+    /**
+     *  \brief          Out Z low register address.
+     */
+    #define `$INSTANCE_NAME`_OUT_Z_L_REGISTER           0x2C
+    
+    /**
+     *  \brief          Out Z high register address.
+     */
+    #define `$INSTANCE_NAME`_OUT_Z_H_REGISTER           0x2D
+    
+    /**
+     *  \brief          FIFO control register address.
+     */
+    #define `$INSTANCE_NAME`_FIFO_CTRL_REGISTER         0x2E
+    
+    /**
+     *  \brief          FIFO SRC register address.
+     */
+    #define `$INSTANCE_NAME`_FIFO_SRC_REGISTER          0x2F
+    
+    /**
+     *  \brief          Interrupt 1 configuration register address.
+     */
+    #define `$INSTANCE_NAME`_INT1_CFG_REGISTER          0x30
+    
+    /**
+     *  \brief          Interrupt 1 source register address.
+     */
+    #define `$INSTANCE_NAME`_INT1_SRC_REGISTER          0x31
+    
+    /**
+     *  \brief          Interrupt 1 threshold register address.
+     */
+    #define `$INSTANCE_NAME`_INT1_THS_REGISTER          0x32
+    
+    /**
+     *  \brief          Interrupt 1 duration register address.
+     */
+    #define `$INSTANCE_NAME`_INT1_DURATION_REGISTER     0x33
+    
+    /**
+     *  \brief          Interrupt 2 configuration register address.
+     */
+    #define `$INSTANCE_NAME`_INT2_CFG_REGISTER          0x34
+    
+    /**
+     *  \brief          Interrupt 2 source register address.
+     */
+    #define `$INSTANCE_NAME`_INT2_SRC_REGISTER          0x35
+    
+    /**
+     *  \brief          Interrupt 2 threshold register address.
+     */
+    #define `$INSTANCE_NAME`_INT2_THS_REGISTER          0x36
+    
+    /**
+     *  \brief          Interrupt 2 duration register address.
+     */
+    #define `$INSTANCE_NAME`_INT2_DURATION_REGISTER     0x37
+    
+    /**
+     *  \brief          Click configuration register address.
+     */
+    #define `$INSTANCE_NAME`_CLICK_CFG_REGISTER         0x38
+    
+    /**
+     *  \brief          Click source register address.
+     */
+    #define `$INSTANCE_NAME`_CLICK_SRC_REGISTER         0x39
+    
+    /**
+     *  \brief          Click threshold register address.
+     */
+    #define `$INSTANCE_NAME`_CLICK_THS_REGISTER         0x3A
+    
+    /**
+     *  \brief          Time limit register address.
+     */
+    #define `$INSTANCE_NAME`_TIME_LIMIT_REGISTER        0x3B
+    
+    /**
+     *  \brief          Time latency register address.
+     */
+    #define `$INSTANCE_NAME`_TIME_LATENCY_REGISTER      0x3C
+    
+    /**
+     *  \brief          Time window register address.
+     */
+    #define `$INSTANCE_NAME`_TIME_WINDOW_REGISTER       0x3D
+    
+    /**
+     *  \brief          Active threshold register address.
+     */
+    #define `$INSTANCE_NAME`_ACT_THS_REGISTER           0x3E
+    
+    /**
+     *  \brief          Active duration register address.
+     */
+    #define `$INSTANCE_NAME`_ACT_DUR_REGISTER           0x3F
+
+    #ifdef __cpluscplus
+        }
+    #endif
+    
+#endif
+/* [] END OF FILE */
